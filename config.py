38c816e00aa2dd4e753b986e64bc7ab7bce67f2c
Ashbt <ashley.beattie@gmail.com>
2020-02-28 23:49:23 -0500

    updated the inception weights

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
+import os
+
 """Global configuration."""
 
 # ----------------------------------------------------------------------------
-# Paths.
+# Paths.'msg-stylegan-tf'
+repo_name = "msg-stylegan-tf"
+parent_repo_dir = os.path.abspath(os.curdir).split(repo_name)[0] + repo_name + "/"
 
-result_dir = "/home/karnewar/home2/self/msg-stylegan/"
-data_dir = "/media/deepstorage01/datasets_external/"
-cache_dir = "/home/karnewar/home2/self/msg-stylegan/cache"
-run_dir_ignore = ["results", "datasets", "cache"]
+
+result_dir = parent_repo_dir + "/results"
+data_dir = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/stylegan-nc24/code/data/CanadianaLandscapes/1_tfrecord"
+cache_dir = parent_repo_dir + "/cache"
+run_dir_ignore = ["results", "data", "cache"]
 
 # ----------------------------------------------------------------------------
