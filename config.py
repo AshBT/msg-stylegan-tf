2217b8f0beab1107fb03c858dda5cd33a4e50f0f
Ashbt <ashley.beattie@gmail.com>
2020-02-29 00:05:40 -0500

    updated the data directory

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ parent_repo_dir = os.path.abspath(os.curdir).split(repo_name)[0] + repo_name + "
 
 
 result_dir = parent_repo_dir + "/results"
-data_dir = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/stylegan-nc24/code/data/CanadianaLandscapes/1_tfrecord"
+data_dir = "/mnt/batch/tasks/shared/LS_root/mounts/clusters/stylegan-nc24/code/"
 cache_dir = parent_repo_dir + "/cache"
 run_dir_ignore = ["results", "data", "cache"]
 
